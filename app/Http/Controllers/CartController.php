2bf72402dffb8a3eafdf356a03e8c3c417873c11
Audri511 <audrimelinamuthikatidjan@gmail.com>
2025-10-19 16:36:13 +0700

    cart page

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index(Request $request)
+    {
+        // Ambil cart dari session atau buat default kalau belum ada
+        $cartItems = $request->session()->get('cartItems', [
+            ['id' => 1, 'nama' => 'Sofa', 'quantity' => 2, 'harga' => 2000000],
+            ['id' => 2, 'nama' => 'Sofa Bed', 'quantity' => 1, 'harga' => 3500000],
+            ['id' => 3, 'nama' => 'Meja Makan', 'quantity' => 1, 'harga' => 1500000],
+        ]);
+
+        // Hitung total
+        $totalHargaCart = 0;
+        foreach ($cartItems as &$item) {
+            $item['total_harga'] = $item['harga'] * $item['quantity'];
+            $totalHargaCart += $item['total_harga'];
+        }
+
+        // Simpan lagi ke session (update)
+        $request->session()->put('cartItems', $cartItems);
+
+        return view('page.cart', compact('cartItems', 'totalHargaCart'));
+    }
+
+    public function update(Request $request, $type, $id)
+    {
+        $cartItems = $request->session()->get('cartItems', []);
+
+        foreach ($cartItems as &$item) {
+            if ($item['id'] == $id) {
+                $item['quantity'] += ($type === 'plus') ? 1 : -1;
+                if ($item['quantity'] < 1) {
+                    $item['quantity'] = 1;
+                }
+                $item['total_harga'] = $item['harga'] * $item['quantity'];
+                break;
+            }
+        }
+
+        $request->session()->put('cartItems', $cartItems);
+
+        return redirect()->route('cart.index');
+    }
+
+    public function remove(Request $request, $id)
+    {
+        $cartItems = $request->session()->get('cartItems', []);
+
+        $cartItems = array_filter($cartItems, function ($item) use ($id) {
+            return $item['id'] != $id;
+        });
+
+        $request->session()->put('cartItems', $cartItems);
+
+        return redirect()->route('cart.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
     return view('page.home');
 });
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::get('/cart/update/{type}/{id}', [CartController::class, 'update'])->name('cart.update');
+Route::get('/cart/delete/{id}', [CartController::class, 'remove'])->name('cart.delete');
